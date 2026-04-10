422589e0a7aeb15077cef5c8b3bbf266bb15b285
AJAYBIR621 <ajaybirs899@gmail.com>
2026-04-10 12:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -72,9 +72,9 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+N08m5L1mCTU2NTE1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/heartstealer_x")
-SUPPORT_GROUP = "https://t.me/heartstealer_x"
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ABOUTxYUTAA")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+NQGCtLEJJEo3OWY1")
+SUPPORT_GROUP = "https://t.me/+NQGCtLEJJEo3OWY1"
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
